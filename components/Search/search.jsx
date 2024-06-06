f1b3a6972a3e36a27435309c7fe6844d9fa41731
Kasten33 <hurlrykaden@gmail.com>
2024-06-05 18:34:29 -0600

    working on backend simply becuse it doesnt want to work

--- components/Search/search.jsx
+++ components/Search/search.jsx
@@ -3,7 +3,6 @@ import Styles from "./Search.module.scss";
 const Search = () => {
   return (
     <div className={Styles.search}>
-      <h1>Search Page</h1>
       <p>Search for books here!</p>
     </div>
   );
